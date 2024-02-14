10cd4f2e1e753a52651512e67fc2075ef3ce5b8c
Alexjkz <aespomobile@gmail.com>
2024-02-14 18:32:20 +0100

    OK

--- Assets/01_Scripts/PlayerMovement.cs
+++ Assets/01_Scripts/PlayerMovement.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using TMPro;
+using UnityEditor.UI;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Windows.Speech;
@@ -18,50 +19,53 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private GameObject pannelloSconfitta;
     [SerializeField] private GameObject pannelloVincita;
+    [SerializeField] private float fallingTreshold = -5.0f;
 
 
     private Rigidbody2D rigidbodyMario;
+    private Transform marioPosition;
+    private float marioYAxis;
     private PlayerControls controls;
     private Animator animatorMario;
     private int statoMario;
-
+    private bool isJumping = false;
     private bool _jumpInput;
     private float _moveInput;
     private bool reverse = false;
 
 
 
-
-
     void Awake()
     {
-        
-        //input = new MyInputSystem();
-
         controls = new PlayerControls();
 
         rigidbodyMario = GetComponent<Rigidbody2D>();
         animatorMario = GetComponent<Animator>();
         statoMario = animatorMario.GetInteger("StateMarioRed");
+        marioPosition = GetComponent<Transform>();
 
         controls.MyInputSystem.Movement.performed += ctx => _moveInput = ctx.ReadValue<float>();
         controls.MyInputSystem.Movement.canceled += ctx => _moveInput = 0.0f;
 
         controls.MyInputSystem.Jump.performed += ctx => _jumpInput = true;
         controls.MyInputSystem.Jump.canceled += ctx => _jumpInput = false;
-
-        //_moveInput = 1.0f;
     }
+
+
 
     void OnEnable()
     {
         controls.Enable();
     }
 
+
+
     void OnDisable()
     {
         controls.Disable();
     }
+
+
 
     void Start()
     {
@@ -73,19 +77,12 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-
-        Debug.Log(_jumpInput);
-        Debug.Log(_moveInput);
-
-
         // Movimento
         Vector3 movement = new Vector3(_moveInput, 0.0f, 0.0f);
-
-        // Animazioni movimento
         transform.Translate(movement * _speed * Time.deltaTime);
 
-        // Animazioni movimento
-        if(isItGrounded() && _moveInput > 0.0f)
+        // Animazioni
+        if(isItGrounded() && _moveInput > 0.0f && isJumping == false)
         {
             animatorMario.SetInteger("StateMarioRed",1);
             if(reverse == true)
@@ -93,41 +90,59 @@ public class PlayerMovement : MonoBehaviour
                 transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
                 reverse = false;
             }
-            
             reverse = false;
         }
-        else if(isItGrounded() && _moveInput < 0.0f)
+        else if(isItGrounded() && _moveInput < 0.0f && isJumping == false)
         {
             animatorMario.SetInteger("StateMarioRed",1);
             if(reverse == false)
             {
                 transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
                 reverse = true;
-            }
-            
+            }      
         }
-        else if(isItGrounded() == false)
+        else if(isItGrounded() && _moveInput == 0.0f && isJumping == false)
         {
-            animatorMario.SetInteger("StateMarioRed",2);
+            animatorMario.SetInteger("StateMarioRed",0);      
         }
         else
         {
-            animatorMario.SetInteger("StateMarioRed",0);
+            Debug.Log("Non sono grounded");
         }
 
         // Salto
         if(isItGrounded() && _jumpInput)
         {
             rigidbodyMario.AddForce(new Vector3(0.0f, _jumpForce, 0.0f), ForceMode2D.Impulse);
+            isJumping = true;
+            StartCoroutine(jumpAnimation());
             _jumpInput = false;
+            
         }
         Debug.Log(isItGrounded());
-
         Debug.DrawRay(transform.position, Vector2.down * groundCheckDistance, Color.red);
-
+        
+        // Morte
+        marioYAxis = marioPosition.position.y;
+        Debug.Log(marioYAxis);
+        if(marioYAxis < fallingTreshold)
+        {
+            YouLost();
+        }
     }
 
-    
+
+
+    private IEnumerator jumpAnimation()
+    {
+        animatorMario.SetInteger("StateMarioRed",2);
+        yield return new WaitForSeconds(0.5f);
+        animatorMario.SetInteger("StateMarioRed",0);
+        isJumping = false;
+    }
+ 
+
+
     public bool isItGrounded()
     {
         if(Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, groundLayer))
@@ -140,43 +155,24 @@ public class PlayerMovement : MonoBehaviour
             Debug.Log("IsNotGrounded");
             return false;
         }
-
     }
+
+
 
     void OnCollisionEnter2D(Collision2D other)
     {
         if(other.collider.tag == "NutriaDamage")
         {
-            Debug.Log("YOU LOST");
-            Time.timeScale = 0f;
-            pannelloSconfitta.SetActive(true);
-            
+            YouLost();         
         }
     }
 
-    //---- ALTRO ----
 
-    // private void OnDrawGizmos()
-    // {
-    //     Gizmos.DrawWireCube(transform.position-transform.up * groundCheckDistance, boxSize);
-    // }
-    
 
-    // private void OnCollisionEnter2D(Collision2D other)
-    // {
-        
-    //     if(other.gameObject.CompareTag("Terreno"))
-    //     {
-    //         isGrounded = true;
-    //     }
-    // }
-
-    // private void OnCollisionExit2D(Collision2D other)
-    // {
-    //     Debug.Log("DIOBESTIA");
-    //     if(other.gameObject.CompareTag("Terreno"))
-    //     {
-    //         isGrounded = false;
-    //     }
-    // }
+    private void YouLost()
+    {
+        Debug.Log("YOU LOST");
+        Time.timeScale = 0f;
+        pannelloSconfitta.SetActive(true); 
+    }
 }
